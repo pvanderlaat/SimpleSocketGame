6c03e97282bb4f9b243d119a8bee3aed8b28227c
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-02-09 16:52:32 -0500

    Unpolished prototype

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 
+const words = require("./words.js")
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 const PORT = process.env.PORT || 3000;
+const timer = ms => new Promise(res => setTimeout(res, ms));
+
 
 app.use(express.static('public'));
 
@@ -15,6 +19,9 @@ server.listen(PORT, () => {
 });
 
 const players = {};
+var alive = [];
+var safe = [];
+var word = "";
 
 io.on('connection', (socket) => {
   // Generate a random color for the player
@@ -27,17 +34,32 @@ io.on('connection', (socket) => {
     y: Math.floor(Math.random() * 10) * 50,
     color,
     name: '',
-    message: ''
+    message: '',
+    state: "dead",
+    wins: 0
   };
+
 
   // Add the player to the players object
   players[socket.id] = player;
 
   // Emit the initial game state to the newly connected player
-  socket.emit('init', { players });
+  socket.emit('init', { players, word });
 
-  // Broadcast the new player to all connected clients
+  // Broadcast the new player to all other connected clients
   socket.broadcast.emit('newPlayer', player);
+
+  for (var id of Object.keys(players)) {
+    if (players[id].state == 'alive') {
+      alive.push(id);
+    } else if (players[id].state == 'safe') {
+      safe.push(id);
+    }
+  }
+
+  if (Object.keys(players).length == 1) {
+    startGame();
+  }   
 
   // Handle player movement
   socket.on('move', (direction) => {
@@ -45,10 +67,46 @@ io.on('connection', (socket) => {
   });
 
   socket.on('chat message', (msg) => {
-    const playerId = socket.id;
+    var playerId = socket.id;
+
+    
+    if (alive.indexOf(playerId) == -1 || word == "loading") {
+      return;
+    }
     players[playerId].message = msg;
-    io.emit('messageSent', { playerId, msg });
+    var correct = word == msg;
+    io.emit('messageSent', {playerId, msg, correct});
+
+    console.log(players[socket.id].name, playerId, alive, safe)
+
+    alive.splice(alive.indexOf(playerId), 1);
+    players[playerId].state = "dead";
+    if (correct) {
+      safe.push(playerId)
+      players[playerId].state = "safe";
+    }
+
+    if (alive.length == 1) {
+      console.log("culling the weak")
+      playerId = alive[0];
+      console.log(playerId, id)
+      var msg = "lol";
+      var correct = false;
+      io.emit('messageSent', {playerId, msg, correct});
+      players[id].state = "dead";
+      alive.splice(alive.indexOf(playerId), 1);
+    }
+
+    if (alive.length == 0) {
+      if (safe.length > 1) {
+        newWord();
+      } else {
+        io.emit("newWord", "loading...")
+        setTimeout(() => startGame(), 5000);
+      }
+    }
   });
+
 
   socket.on('nameGiven', (name) => {
     const playerId = socket.id;
@@ -77,4 +135,30 @@ function handlePlayerMovement(playerId, direction) {
   }
 
   io.emit('playerMoved', { playerId, x: player.x, y: player.y });
+}
+
+async function startGame() {
+  console.log("restarting game...")
+  alive = []
+  for (const playerId of Object.keys(players)) {
+    msg = ""
+    correct = true;
+    io.emit('messageSent', {playerId, msg, correct});
+    alive.push(playerId);
+    players[playerId].state = "alive"
+  }
+  safe = [];
+  io.emit("reviveAll");
+  newWord();
+}
+
+function newWord() {
+  for (const id of safe) {
+    safe.splice(safe.indexOf(id), 1);
+    alive.push(id);
+    players[id].state = "alive";
+  }
+
+  word = words[Math.floor(Math.random() * 9884)]
+  io.emit("newWord", word)
 }
